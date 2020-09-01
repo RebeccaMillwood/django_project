247ba5a021bce5aa457a2a1de070226eb8e73359
RebeccaMillwood <bec-millwood@live.com.au>
2020-09-01 21:00:54 +1000

    removed edit story - wasn't working and want to start again

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -14,12 +14,3 @@ class StoryForm(ModelForm):
             'type':'date'}),
         }
 
-class EditStory(ModelForm):
-    class Meta:
-        model = NewsStory
-        fields = ['title', 'pub_date', 'content', 'image']
-        widgets = {
-            'pub_date': forms.DateInput(format=('%m/%d/%Y'),
-            attrs={'class':'form-control', 'placeholder':'Select a date',
-            'type':'date'}),
-        }
--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -6,6 +6,5 @@ app_name = 'news'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('add-story/', views.AddStoryView.as_view(), name='newStory'),
-    path('edit-story/', views.EditStoryView.as_view(), name='editStory')
+    path('add-story/', views.AddStoryView.as_view(), name='newStory')
 ]
